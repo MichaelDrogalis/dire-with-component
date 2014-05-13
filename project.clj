b8784dc67101eed22124d0cf863db114978b30f1
Michael Drogalis <mjd3089@rit.edu>
2014-05-12 21:27:29 -0400

    Add example.g

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject dire-with-component "0.1.0-SNAPSHOT"
+  :description "Dire with Component"
+  :url "https://github.com/MichaelDrogalis/dire-with-component"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [com.stuartsierra/component "0.2.1"]
+                 [dire "0.5.2"]])
